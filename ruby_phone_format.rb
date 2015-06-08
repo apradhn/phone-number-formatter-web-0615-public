d3fa2bff556a90ad1e62ef4b3fd9cd5b8c32dbb1
apradhn <abhijit.pradhan6@gmail.com>
2015-06-08 14:14:49 -0400

    all done

--- ruby_phone_format.rb
+++ ruby_phone_format.rb
@@ -1,1 +1,8 @@
-# Write your code here
+def normalize_phone_number(phone_number)
+  numbers = phone_number.scan(/\d/)
+  if phone_number =~ /\(?\d{3}[\) -]?\d{3}[- ]?\d{4}/
+    normalized_phone_number = "(#{numbers[0..2].join}) #{numbers[3..5].join}-#{numbers[6..10].join}"
+  else
+    phone_number
+  end  
+end
